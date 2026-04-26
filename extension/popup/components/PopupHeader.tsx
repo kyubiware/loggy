716cf27180211effba8ca8f3200e3a6cfa48977c
Kyubiware <kyubiware@gmail.com>
2026-04-26 15:13:17 -0700

    refactor: update PopupHeader component and tsconfig for improved structure

--- extension/popup/components/PopupHeader.tsx
+++ extension/popup/components/PopupHeader.tsx
@@ -1,5 +1,7 @@
 import type React from 'react'
 
+import iconUrl from '../../icons/icon48.png'
+
 export interface PopupHeaderProps {
   connected: boolean
 }
@@ -10,9 +12,7 @@ export function PopupHeader({
   return (
     <div className='flex items-center justify-between border-b border-stone-200 dark:border-stone-700 pb-2'>
       <h1 className='text-lg font-semibold flex items-center gap-2'>
-        <span role='img' aria-label='Loggy'>
-          🪵
-        </span>{' '}
+        <img src={iconUrl} alt='Loggy' className='w-6 h-6' />
         Loggy
       </h1>
     </div>
